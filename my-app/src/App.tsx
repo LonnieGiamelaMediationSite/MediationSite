451cf0008d45bb3c15647ac651b1e9a90683f164
“Nader <nadmot23@gmail.com>
2024-06-24 10:26:53 -0700

    Initial Commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Lonnie Giamela Mediation Site - Building soon</h1>
+        <h1>Lonnie Giamela Mediation Site - Building soon!</h1>
       </div>
     </>
   );
